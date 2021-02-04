4e407b2221e6a7218151973ca88558f583479597
DESKTOP-1L84BIH\Ryan <RyanC.Barr@gmail.com>
2021-02-03 18:30:07 -0800

    Added DB configuration to Constants.cs

--- Mine/Mine/Constants.cs
+++ Mine/Mine/Constants.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+
 
 namespace Mine
 {
-    class Constants
+    public static class Constants
     {
+        public const string DatabaseFilename = "TodoSQLite.db3";
+
+
+        public const SQLite.SQLiteOpenFlags Flags =
+
+            //Open DB in Read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+
+            //Create DB is it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+
+            //Enable Multi0threaded DB access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+
     }
 }
