86a858ee9f4ebd33658995a43be737fbee4001f4
DESKTOP-1L84BIH\Ryan <RyanC.Barr@gmail.com>
2021-02-10 04:36:23 -0800

    Added RollDice Incalid Roll Zero to DiceHelperTests.cs

---
+++ UnitTests/Helpers/DiceHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Mine.Helpers;
+using Mine.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class DiceHelperTests
+    {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+        }
+    }
+
+}
